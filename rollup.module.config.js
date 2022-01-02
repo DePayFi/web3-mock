66fcaf82e4a21990081e1c24a29afbfe1ec73d3a
Sebastian Pape <s@pape.io>
2022-01-02 17:27:30 +0100

    v11.0.0: drop cjs support and focus on umd+esm js packages

--- rollup.module.config.js
+++ rollup.module.config.js
@@ -10,14 +10,9 @@ export default {
   input: 'src/index.js',
   output: [
     {
-      format: 'cjs',
-      globals: globals,
-      file: 'dist/cjs/index.js'
-    },
-    {
       format: 'es',
       globals: globals,
-      file: 'dist/es/index.js'
+      file: 'dist/esm/index.js'
     },
     {
       format: 'umd',
