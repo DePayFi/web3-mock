cd071c2255d88567da87e6824902ac72e7262da2
Sebastian Pape <s@pape.io>
2023-03-20 12:03:06 +0100

    v13.25.5: polyfill fetch

--- tests/setup.js
+++ tests/setup.js
@@ -1,1 +1,16 @@
 import 'regenerator-runtime/runtime'
+
+import fetch, {
+  Blob,
+  blobFrom,
+  blobFromSync,
+  File,
+  fileFrom,
+  fileFromSync,
+  FormData,
+  Headers,
+  Request,
+  Response,
+} from 'node-fetch'
+
+global.fetch = fetch
